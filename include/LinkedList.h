4ddf2fa469616ea00a962d226006884861f6aaa2
JoshuaM928 <joshua.muhammad928@gmail.com>
2025-05-05 02:09:05 -0700

    intial commit

---
+++ include/LinkedList.h
@@ -0,0 +1,14 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "Node.h"
+
+class LinkedList {
+    public:
+    
+    private:
+        int length;
+        Node *head;
+        Node *tail;
+}
+
+#endif
---
+++ include/Node.h
@@ -0,0 +1,14 @@
+#ifndef NODE_H
+#define NODE_H
+#include <string>
+
+class Node {
+    public:
+
+    private:
+        // Data stored in node
+        Node *next;
+        Node *prev; 
+}
+
+#endif
