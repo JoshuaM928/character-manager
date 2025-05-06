a2ffdeb4fe700c4618344f94f6ac23988fbc8ce8
JoshuaM928 <joshua.muhammad928@gmail.com>
2025-05-06 15:54:57 -0700

    add Node definition & remove src/.gitkeep

--- include/models/Node.h
+++ include/models/Node.h
@@ -4,9 +4,20 @@
 
 class Node {
     public:
-
+        // Default constructor, copy constructor, & deconstructor
+            Node();
+            Node(Actor *other);
+            ~Node();
+        // Getters
+            Actor* getData();
+            Node* getNext();
+            Node* getPrev();
+        // Setters
+            void setData(Actor *data);
+            void setNext(Node *next);
+            void sePrev(Node *prev);
     private:
-        // Data stored in node
+        Actor *data;
         Node *next;
         Node *prev; 
 }
