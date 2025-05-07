798580de3cb65a41112f12302b6d1bc880b3e7b4
JoshuaM928 <joshua.muhammad928@gmail.com>
2025-05-07 02:28:32 -0700

    add LinkedList definition

--- include/models/LinkedList.h
+++ include/models/LinkedList.h
@@ -4,11 +4,26 @@
 
 class LinkedList {
     public:
-    
+        // Default constructor, copy constructor, & deconstructor
+            LinkedList();
+            LinkedList(const LinkedList &other);
+            ~LinkedList();
+        // Getters
+            Node* getHead() const;
+            Node* getTail() const;
+            int getSize() const;
+        // Setters
+            void setSize(const int newSize);
+            void setHead(Node *newHead);
+            void setTail(Node *newTail);
+        // Actions
+            Node* linearSearch(Actor *target) const;
+            void mergeSort();
     private:
-        int length;
-        Node *head;
-        Node *tail;
-}
+        // Members
+            int size;
+            Node *head;
+            Node *tail;
+};
 
-#endif
+#endif // LINKEDLIST_H
