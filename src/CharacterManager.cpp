a745108c9cc26a9462f39035cab77cf75dae2774
Kojiro Miura <kmiura@sfsu.edu>
2025-05-13 04:15:34 +0000

    I think I fixed everything i could

--- src/CharacterManager.cpp
+++ src/CharacterManager.cpp
@@ -22,6 +22,7 @@ void CharacterManager::run() {
     do {
         displayMenu();
         cin >> choice;
+        cin.ignore();
         handleUserInput(choice);
     } while (choice != 0);
 }
@@ -43,12 +44,28 @@ void CharacterManager::handleUserInput(int choice) {
         string name; 
         char type;
 
-        cout << "Enter Character Name:  ";
-        getline(cin, name);
+        cout << "Enter Character Name: ";
+        while (name.empty()) 
+        {
+            getline(cin, name);
+            if (name.empty()) 
+            {
+            cout << "Name cannot be empty. Try again: ";
+            }
+        }
 
-        cout << "Is your character a Player(P), Ally(A), Neutral(N), or Hostile(H)?:  ";
-        cin >> type;
+        while(toupper(type) != 'P' && toupper(type) != 'A' && toupper(type) != 'N' && toupper(type) != 'H')
+        {
+            cout << "Is your character a Player(P), Ally(A), Neutral(N), or Hostile(H)?:  ";
+            cin >> type;
+            if(toupper(type) != 'P' && toupper(type) != 'A' && toupper(type) != 'N' && toupper(type) != 'H')
+            {
+                cout << "Invalid response, please Enter P, A, N, or H\n";
+            }
+
+        }
         cin.ignore();
+        cout << endl;
 
         bool isPlayer = false, isAlly = false, isNeutral = false, isHostile = false;
 
@@ -56,33 +73,44 @@ void CharacterManager::handleUserInput(int choice) {
         case 'P':
         {
             isPlayer = true;
+            cout << "Character is a Player\n";
             break;
         }
         case 'A':
         {
             isAlly = true;
+            cout << "Character is an Ally\n";
             break;
         }
         case 'N':
         {
             isNeutral = true;
+            cout << "Character is a Neutral\n";
             break;
         }
         case 'H': 
         {
             isHostile = true;
+            cout << "Character is a Hostile\n";
             break;
         }
         default: isNeutral = true;
         }
         CharacterCreationHandeler::createCharacter(characterList, name, isPlayer, isAlly, isNeutral, isHostile);
-        cout << "Character added successfully!\n";
+        cout << "Character \"" << name <<"\" added successfully!\n";
         break;
     }
     case REMOVE: {
         string name;
-        cout << "Enter character name to remove: ";
-        getline(cin, name);
+        cout << "Enter the Character's name you want to Remove: ";
+        while (name.empty()) 
+        {
+            getline(cin, name);
+            if (name.empty()) 
+            {
+            cout << "Name cannot be empty. Try again: ";
+            }
+        }
         if (CharacterCreationHandeler::removeCharacter(characterList, name)) {
             std::cout << "Character removed successfully!\n";
         }
