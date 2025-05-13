07012e18b7ef8bab31f278ead7ca355968f711fe
JoshuaM928 <joshua.muhammad928@gmail.com>
2025-05-12 18:02:35 -0700

    add Magic and Physical skills definitions

---
+++ include/models/skills/Magic.h
@@ -0,0 +1,13 @@
+#ifndef MAGIC_H
+#define MAGIC_H
+
+class Magic {
+    public: 
+        // Magic spells
+            void fireBall();
+            void lightingBolt();
+            void raiseTheDead();
+    private:
+};
+
+#endif
---
+++ include/models/skills/Physical.h
@@ -0,0 +1,14 @@
+#ifndef PHYSICAL_H
+#define PHYSICAL_H
+
+class Physical {
+    public:
+        // Physical skills
+            void heavyPunch();
+            void lightPunch();
+            void heavyKick();
+            void lightKick();
+    private:
+}
+
+#endif
